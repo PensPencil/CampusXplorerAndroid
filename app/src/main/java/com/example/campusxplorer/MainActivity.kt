318a69d29e40e7b4e7b6163c86dc0a8dba210e61
PensPencil <kangarooratwithquill@gmail.com>
2023-11-07 13:56:00 +0530

    project base

---
+++ app/src/main/java/com/example/campusxplorer/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.campusxplorer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mapbox.maps.MapView
+import com.mapbox.maps.Style
+
+
+var mapView: MapView? = null
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        mapView = findViewById(R.id.mapView)
+        mapView?.getMapboxMap()?.loadStyleUri(Style.DARK)
+    }
+    override fun onStart() {
+        super.onStart()
+        mapView?.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView?.onStop()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView?.onLowMemory()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView?.onDestroy()
+    }
+}
